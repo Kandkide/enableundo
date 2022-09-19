72ac9ba75339dbbac6a9b0d01430876995b8cc1b
kandkide <kandkide@gmail.com>
2022-09-19 19:29:55 +0900

    v100

--- undoredo_example2.py
+++ undoredo_example2.py
@@ -17,19 +17,17 @@ class Tokyo(EnableUndoRedo):
     def show(self):
         print(f"{self.hour}:00 {self.weather}")
 
-def main():
-    tokyo = Tokyo()
+
+tokyo = Tokyo()
+tokyo.show()
+
+for i in range(23):
+    if i % 6 == 0:
+        tokyo.state = i
+    tokyo.timepassed()
+
+print()
+
+for i in range(4):
+    tokyo.undo()
     tokyo.show()
-
-    for i in range(23):
-        if i % 6 == 0:
-            tokyo.state = i
-        tokyo.timepassed()
-
-    print()
-
-    for i in range(4):
-        tokyo.undo()
-        tokyo.show()
-
-main()
